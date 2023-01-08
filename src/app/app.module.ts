a255c1fe90887f6a39f4368645e757015a1bf1e0
ArianaMed <mdelfina136@gmail.com>
2023-01-08 17:18:46 -0300

    cambioss

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EncabezadoComponent } from './componetes/encabezado/encabezado.component';
+import { DatospersonalesComponent } from './componetes/datospersonales/datospersonales.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    EncabezadoComponent
+    EncabezadoComponent,
+    DatospersonalesComponent
   ],
   imports: [
     BrowserModule,
